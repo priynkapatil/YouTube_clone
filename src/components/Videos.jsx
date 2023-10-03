8e5547b4278a095822292158324f1dd48cffc2f1
priynkapatil <priyankapatilp22@gmail.com>
2023-10-03 18:28:21 +0530

    Initial commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,24 @@
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import { VideoCard, ChannelCard } from "./index";
+
+const Videos = ({ Videos }) => {
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={4}
+    >
+      {Videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
